76ced669d8d0f5e5c89f19e8ffb0972a42479f5e
Michael McDermott <mcdermott.michaelj@gmail.com>
2024-03-27 00:23:40 -0500

    Removing JCL.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
+if [[ ! "$PATH" == */home/michael/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/michael/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "${HOME}/.fzf/shell/key-bindings.zsh"
+eval "$(fzf --zsh)"
